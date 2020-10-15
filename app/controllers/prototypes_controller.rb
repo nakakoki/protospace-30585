1fd63c8963fee241190ec1303731b271bb124007
nakakoki <71761751+nakakoki@users.noreply.github.com>
2020-10-15 15:23:20 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, only: [:edit,]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.includes(:user)
